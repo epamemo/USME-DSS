242084107cec9de55acda6c0dc01bfc83f9bc711
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-14 15:26:50 +0700

    edit add form for input nilai data

--- nilai_kriteria.php
+++ nilai_kriteria.php
@@ -109,6 +109,169 @@ include 'header.php';
         }
         ?>
     </table>
+    <br>
+    <h1>Input Data Perhitungan</h1>
+    <button type="submit" class="btn btn-success" data-toggle="modal" data-target="#myModalAddNilai">
+        Tambah Nilai
+    </button>
+    <br>
+    <table class=" table table-striped table-hover">
+        <tr>
+            <th>Data Alternatif</th>
+            <?php
+            $queryk = mysqli_query($koneksi, "SELECT * FROM kriteria");
+            while ($k = mysqli_fetch_array($queryk)) { ?>
+                <th><?php echo $k['nama']; ?></>
+                <?php } ?>
+                <th>Opsi</th>
+        </tr>
+        <?php
+        $no = 1;
+        $query = mysqli_query($koneksi, "SELECT * FROM alternatif");
+        $queryn = mysqli_query($koneksi, "SELECT * FROM nilai_alternatif");
+        while ($d = mysqli_fetch_array($query)) { ?>
+            <tr>
+                <td><?php echo $d['nama']; ?></td>
+                <?php while ($q = mysqli_fetch_array($queryn)) {
+                    echo "<td>" . $q['nilai'] . "</td>";
+                } ?>
+                <td>
+                    <a href="#" type="button" class="btn btn-info btn-xs" data-toggle="modal" data-target="#myModal<?php echo $d['id']; ?>">Edit</a>
+                    <a href="#" type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myDelete<?php echo $d['id']; ?>">Hapus</a>
+                </td>
+            </tr>
+            <!-- Modal Edit -->
+            <div class="modal fade" id="myModal<?php echo $d['id']; ?>" role="dialog">
+                <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Edit Data Alternatif</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <form role="form" action="alternatif.php" method="POST">
+                                <?php
+                                $id = $d['id'];
+                                $query_edit = mysqli_query($koneksi, "SELECT * FROM alternatif WHERE id='$id'");
+                                while ($row = mysqli_fetch_array($query_edit)) {
+                                ?>
+                                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                    <input type="hidden" name="table" value="<?php echo "alternatif" ?>">
+                                    <div class="form-group">
+                                        <label>Nama</label>
+                                        <input type="text" name="nama" class="form-control" value="<?php echo $row['nama']; ?>">
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" name="edit" class="btn btn-success">Edit</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal Delete -->
+            <div class="modal fade" id="myDelete<?php echo $d['id']; ?>" role="dialog">
+                <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Edit Data Alternatif</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <form role="form" action="alternatif.php" method="POST">
+                                <?php
+                                $id = $d['id'];
+                                $query_edit = mysqli_query($koneksi, "SELECT * FROM alternatif WHERE id='$id'");
+                                while ($row = mysqli_fetch_array($query_edit)) {
+                                ?>
+                                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                    <input type="hidden" name="table" value="alternatif">
+                                    <p class="">Anda yakin ingin menghapus <?php echo $row['nama']; ?>?</p>
+                                    <div class="modal-footer">
+                                        <button type="submit" name="delete" class="btn btn-danger">Hapus</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </table>
+</div>
+<!-- Modal Add Nilai -->
+<div class="modal fade" id="myModalAddNilai" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Nilai Data</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <form role="form" action="nilai_kriteria.php" method="POST">
+                    <div class="form-group">
+                        <select class="form-control" name="id_alternatif" id="id_alternatif">
+                            <option disabled selected> Pilih Data </option>
+                            <?php
+                            $query_add = mysqli_query($koneksi, "SELECT * FROM alternatif");
+                            while ($d = mysqli_fetch_array($query_add)) {
+                            ?>
+                                <option value="<?= $d['id'] ?>"><?= $d['nama'] ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <input type="hidden" name="table" value="nilai_kriteria">
+                    <div class="form-group">
+                        <label>Nilai 1</label>
+                        <input type="text" name="nilai1" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Nilai 2</label>
+                        <input type="text" name="nilai2" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Nilai 3</label>
+                        <input type="text" name="nilai3" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Nilai 4</label>
+                        <input type="text" name="nilai4" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Nilai 5</label>
+                        <input type="text" name="nilai5" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Atribut</label>
+                        <input type="text" name="atribut" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Bobot</label>
+                        <input type="text" name="bobot" class="form-control">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="addNilaiAlternatif" class="btn btn-success">Tambah Data</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- Modal Add -->
 <div class="modal fade" id="myModalAdd" role="dialog">
@@ -173,6 +336,7 @@ include 'header.php';
     </div>
 </div>
 </div>
+
 <!--
 <script>
   function maxLengthCheck(object) {
